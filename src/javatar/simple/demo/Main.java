8bb4943bd3e5d7310e2570a4f0e2f1fcf86895cc
lamh85 <lamh85@gmail.com>
2015-09-21 21:43:59 -0700

    Learning variables.

--- src/javatar/simple/demo/Main.java
+++ src/javatar/simple/demo/Main.java
@@ -3,10 +3,14 @@ package javatar.simple.demo;
 import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) {
-                Scanner sc = new Scanner(System.in);
-                System.out.print("Hey! What's your name: ");
-                String name = sc.nextLine();
-                System.out.println("Hello, " + name + "!");
-        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Hey! What's your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+        int fizz;
+        fizz = 5;
+        System.out.println("The value of fizz is: " + fizz);
+    }
+
 }
